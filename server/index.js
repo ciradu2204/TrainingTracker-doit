a6031247465a4b77e7fa9467aba207e43fbb9c50
Cynthia Iradukunda <c.iradukund@alustudent.com>
2021-11-06 16:45:18 +0200

    connected to database

--- server/index.js
+++ server/index.js
@@ -8,3 +8,13 @@ app.use(express.json({extended: true}));
 app.use(express.urlencoded({extended:true})); 
 
 app.use(cors());
+
+const CONNECTION_URL= 'mongodb+srv://CynthiaIradu:Imananibyose12@trainingtracker-doit.uhzdi.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+
+const PORT = process.env.PORT || 5000;
+
+// connecting to databse, allowing useUnifiedTopology and useNewUrlParser to avoid warning in console
+mongoose.connect(CONNECTION_URL, {useNewUrlParser:true, useUnifiedTopology:true})
+  .then(() => app.listen(PORT, () => console.log("server running on port 3000")))
+  .catch((error) => console.log(error.message));
+
