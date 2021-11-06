0bcbc3110d1ac99f1415944941fe6f4669ac1099
Cynthia Iradukunda <c.iradukund@alustudent.com>
2021-11-06 16:13:22 +0200

    index.js setup

--- server/index.js
+++ server/index.js
@@ -1,4 +1,10 @@
 const express = require('express'); 
-const bodyParser = require('body-parser'); 
 const mongoose = require('mongoose'); 
-const cors = require('cors')
+const cors = require('cors'); 
+
+const app = express(); 
+
+app.use(express.json({extended: true})); 
+app.use(express.urlencoded({extended:true})); 
+
+app.use(cors());
