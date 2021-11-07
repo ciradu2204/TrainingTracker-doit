13278fc907aea0fb9c2abcd824064c5cea4339c3
Cynthia Iradukunda <c.iradukund@alustudent.com>
2021-11-07 17:55:24 +0200

    added the layout

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,43 @@
 import React from "react";
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom"
+import weeklyPlans from "./components/WeeklyPlans/weeklyPlans";
+import Account from "./components/Account/account";
+import Settings from "./components/Settings/settings";
+import Login from "./components/UserAuth/login";
+import Overview from "./components/Overview/overview"; 
+import Layout from "./components/layout";
+import {createTheme, ThemeProvider} from '@mui/material/styles';
 
+const theme  = createTheme({
+palette:{
+    primary:{
+        main: "#7A6AB8",
+        light: "rgb(122, 106, 184, 0.5)",
+        black: "#071544", 
+    }, 
+    secondary:{
+        main:"#D1B8CF",
+        light:"#BAA2B8",
+        black:"rgb(0,0,0, 0.7)"
+    }
+}
+})
 
 const App = () =>{
     return (
-        <div>
-            <h1>App</h1>
-        </div>
+        <ThemeProvider theme={theme}>
+        <Router>
+            <Layout>
+            <Routes>
+                <Route path="/" exact component={Login} />
+                <Route path="/overview" exact component ={Overview}/>
+                <Route path="/weeklyPlans" exact component = {weeklyPlans}/>
+                <Route path="/settings" exact component = {Settings}/>
+                <Route path="/account" exact component = {Account}/>
+            </Routes>
+            </Layout>
+        </Router>
+        </ThemeProvider>
     )
 }
 
